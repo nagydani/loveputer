d4b7b27fdbeb4f064a861a0f812f325b5e546938
aldum <aldum@artixlinux.org>
2023-06-30 19:07:50 +0200

    feat: fix bright colors

--- src/view/color.lua
+++ src/view/color.lua
@@ -1,28 +1,28 @@
 Color = {
-  indices = {
-    black = 0,
-    blue = 1,
-    red = 2,
-    magenta = 3,
-    green = 4,
-    cyan = 5,
-    yellow = 6,
-    white = 7,
-    bright = 8,
-  },
   color = {},
   __index = function(t, c)
-    if t.color[c] then return t.color[c] end
-    local ci = t.indices[c]
-    local bright = ci > 7 and 1 or 0.75
-    local b = ci % 2
-    ci = (ci - b) / 2
-    local r = ci % 2
-    ci = (ci - r) / 2
-    local g = ci % 2
-    t.color[c] = { bright * r, bright * g, bright * b, 1 }
-    return t.color[c]
+    if rawget(Color, c) then return rawget(Color, c) end
+    local bright = c > 7 and 1 or 0.75
+    local oc = c
+    local b = c % 2
+    c = (c - b) / 2
+    local r = c % 2
+    c = (c - r) / 2
+    local g = c % 2
+    Color[oc] = { bright * r, bright * g, bright * b, 1 }
+    return Color[oc]
   end,
+
+  black = 0,
+  blue = 1,
+  red = 2,
+  magenta = 3,
+  green = 4,
+  cyan = 5,
+  yellow = 6,
+  white = 7,
+  bright = 8,
 }
+
 
 setmetatable(Color, Color)
