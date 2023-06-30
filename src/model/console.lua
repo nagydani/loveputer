edab0345f3dd964a6ee206e2a53c36c3111056c7
aldum <aldum@artixlinux.org>
2023-06-30 19:42:45 +0200

    feat: create consoleController

--- src/model/console.lua
+++ src/model/console.lua
@@ -1,10 +1,14 @@
-Counter = {}
+Console = {}
 
-function Counter:new(init)
+function Console:new(init)
   local c = {
     n = init or 0
   }
   setmetatable(c, self)
   self.__index = self
   return c
+end
+
+function Console:incr()
+  self.n = self.n + 1
 end
