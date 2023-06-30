3c8ae076ea855435e7c466da1f68a814acc4f508
aldum <aldum@artixlinux.org>
2023-06-30 17:13:50 +0200

    feat: use consoleView (with HiDPI support)

--- src/main.lua
+++ src/main.lua
@@ -1,11 +1,18 @@
-local model = require("model/counter")
-local view = require("view/counterView")
-local controller = require("controller/counterController")
+local _ = require("model/counter")
+local _ = require("view/consoleView")
+local _ = require("controller/counterController")
 
 function love.load()
+  local hidpi = os.getenv("HIDPI")
+  if hidpi == 'true' or hidpi == 'TRUE' then
+    _G.hiDPI = true
+  end
+
   M = Counter:new()
   C = CounterController:new(M)
-  V = CounterView:new(M)
+  V = ConsoleView:new(M, {
+    fontSize = 24
+  })
 end
 
 function love.update(dt)
