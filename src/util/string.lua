33bf1403bb745ad0b21da53ccf14989a19e25205
aldum <aldum@artixlinux.org>
2023-07-12 18:16:57 +0200

    util: add utf8 index calculator

--- src/util/string.lua
+++ src/util/string.lua
@@ -1,3 +1,5 @@
+local utf8 = require("utf8")
+
 StringUtils = {
   is_non_empty_string = function(s)
     if s and s ~= '' then
@@ -7,5 +9,11 @@ StringUtils = {
       end
     end
     return false
-  end
+  end,
+
+  to_utf8_index = function(s, i)
+    -- local al = utf8.len(s)
+    local ui = utf8.offset(s, i)
+    return ui
+  end,
 }
