73bdf17ab2ab7981e3eb1f97bc313b7e553325f8
aldum <aldum@artixlinux.org>
2023-07-14 15:50:44 +0200

    fix(split): handle starting index over length

--- src/util/string.lua
+++ src/util/string.lua
@@ -39,25 +39,24 @@ StringUtils = {
     return pre, post
   end,
 
-  -- utf8_sub = function(s, i, j)
-  --   local ml = utf8.len(s)
-  --   if ml <= j then j = ml end
-
-  --   i = utf8.offset(s, i)
-  --   j = utf8.offset(s, j + 1) - 1
-  --   return string.sub(s, i, j)
-  -- end,
-
-  utf8_sub_full = function(s, i, j)
+  -- original from http://lua-users.org/lists/lua-l/2014-04/msg00590.html
+  utf8_sub = function(s, i, j)
     i = i or 1
     j = j or -1
     if i < 1 or j < 1 then
       local n = utf8.len(s)
-      if not n then return nil end
+      if not n then return '' end
+      if i > n then return '' end
       if i < 0 then i = n + 1 + i end
-      if j < 0 then j = n + 1 + j end
+      if j < 0 then
+        j = n + 1 + j
+      end
       if i < 0 then i = 1 elseif i > n then i = n end
-      if j < 0 then j = 1 elseif j > n then j = n end
+      if j < 0 then
+        j = 1
+      elseif j > n then
+        j = n
+      end
     end
     if j < i then return "" end
     i = utf8.offset(s, i)
@@ -70,6 +69,4 @@ StringUtils = {
       return ""
     end
   end,
-
-
 }
