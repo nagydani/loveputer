e26c6fe061d26640592b36d936e89c7e24789ce7
aldum <aldum@artixlinux.org>
2023-07-17 18:04:47 +0200

    feat: add multiline input representation

--- tests/input_spec.lua
+++ tests/input_spec.lua
@@ -17,32 +17,33 @@ describe("input model spec", function()
     local test_char2 = 'n'
 
     it('is empty at first', function()
-      assert.same('', model:get_text())
+      assert.same({ '' }, model:get_text())
     end)
 
     it('sets text', function()
       model:set_text(test1)
-      assert.is_equal(test1, model:get_text())
+      assert.same({ test1 }, model:get_text())
     end)
 
     it('clears text', function()
       model:clear()
-      assert.is_equal('', model:get_text())
+      assert.same({ '' }, model:get_text())
     end)
 
     it('appends text', function()
       model:set_text(test1)
-      assert.is_equal(test1, model:get_text())
+      assert.same({ test1 }, model:get_text())
     end)
 
     it('appends characters', function()
       model:add_text(test_char1)
-      assert.is_equal(test1 .. test_char1, model:get_text())
+      assert.same(test1 .. test_char1, model:get_current_line())
       model:clear()
       model:add_text(test_char1)
-      assert.is_equal(test_char1, model:get_text())
+      assert.is_equal(test_char1, model:get_current_line())
       model:add_text(test_char2)
-      assert.is_equal(test_char1 .. test_char2, model:get_text())
+      assert.is_equal(test_char1 .. test_char2,
+        model:get_current_line())
     end)
   end)
 
@@ -55,29 +56,24 @@ describe("input model spec", function()
     local test_char1 = 'x'
 
     it('is at base', function()
-      assert.same('', model:get_text())
+      assert.same({ '' }, model:get_text())
     end)
 
     it('advances by one', function()
       model:add_text(test_char1)
-      local _, cc = model:get_cursor_pos()
+      local cl, cc = model:get_cursor_pos()
       assert.same(2, cc)
+      assert.same(1, cl)
     end)
 
     it('returns on backspace', function()
       model:backspace()
-      assert.same(1, model:get_cursor_x())
+      assert.is_equal(1, model:get_cursor_x())
     end)
 
     it('advances by multiple', function()
       model:add_text(test1)
-      assert.same(1 + #test1, model:get_cursor_x())
-    end)
-
-    it('moves back', function()
-      model:cursor_left()
-      model:cursor_left()
-      assert.same(1 + #test1 - 2, model:get_cursor_x())
+      assert.is_equal(1 + #test1, model:get_cursor_x())
     end)
 
     it('moves forward', function()
@@ -104,13 +100,18 @@ describe("input model spec", function()
       model:cursor_left()
       assert.same(1, model:get_cursor_x())
     end)
+
     it("jumps to the end on End", function()
       model:jump_end()
-      local _, cc = model:get_cursor_pos()
-      local l = #(model:get_text())
-      assert.same(1 + l, cc)
+      local cl, cc = model:get_cursor_pos()
+      local ent = model:get_text()
+      local ll = #(ent[cl]) -- first line length
+      local len = #ent      -- number of lines
+      assert.same(1 + ll, cc)
+      assert.same(cl, len)
     end)
   end)
+
 
   -----------------
   --   UTF-8     --
@@ -127,31 +128,31 @@ describe("input model spec", function()
 
     it('sets UTF-8 text', function()
       model:set_text(test1)
-      assert.is_equal(test1, model:get_text())
-      assert.same(1 + test1_len, model:get_cursor_x())
+      assert.same({ test1 }, model:get_text())
+      assert.is_equal(1 + test1_len, model:get_cursor_x())
     end)
 
     it('clears text', function()
       model:clear()
-      assert.is_equal('', model:get_text())
+      assert.same({ '' }, model:get_text())
     end)
 
     it('appends text', function()
       model:add_text(test1)
-      assert.is_equal(test1, model:get_text())
+      assert.same({ test1 }, model:get_text())
     end)
 
     describe('appends', function()
       model:add_text(test_char2)
       it('UTF-8 characters', function()
-        assert.is_equal(test1 .. test_char2, model:get_text())
+        assert.same({ test1 .. test_char2 }, model:get_text())
       end)
 
       it('ASCII characters', function()
         model:backspace()
-        assert.is_equal(test1, model:get_text())
+        assert.same({ test1 }, model:get_text())
         model:add_text(test_char1)
-        assert.is_equal(test1 .. test_char1, model:get_text())
+        assert.same({ test1 .. test_char1 }, model:get_text())
       end)
     end)
 
@@ -161,7 +162,7 @@ describe("input model spec", function()
       it('', function()
         model:clear()
         model:add_text(test2)
-        assert.is_equal(test2, model:get_text())
+        assert.same({ test2 }, model:get_text())
         local cc = model:get_cursor_x()
         assert.is_equal(line_end, cc)
       end)
@@ -253,7 +254,7 @@ describe("input model spec", function()
         local cc = model:get_cursor_x()
         assert.is_equal(line_end, cc)
         model:delete()
-        assert.is_equal(test2, model:get_text())
+        assert.same({ test2 }, model:get_text())
       end)
 
       it('one', function()
@@ -261,20 +262,22 @@ describe("input model spec", function()
         model:delete()
         local cc = model:get_cursor_x()
         assert.is_equal(line_end - 1, cc)
-        assert.is_equal(string.sub(test2, 1, line_end - 2), model:get_text())
+        assert.same({ string.sub(test2, 1, line_end - 2) },
+          model:get_text())
       end)
 
       it('all', function()
-        local l = #(model:get_text())
-        for i = 1, l do
+        local cl = model:get_cursor_y()
+        local ll = #(model:get_text()[cl])
+        for i = 1, ll do
           model:retreat_cursor()
         end
         local cc = model:get_cursor_x()
         assert.is_equal(1, cc)
-        for i = 1, l do
+        for i = 1, ll do
           model:delete()
         end
-        assert.is_equal('', model:get_text())
+        assert.same({ '' }, model:get_text())
       end)
     end)
 
@@ -283,7 +286,7 @@ describe("input model spec", function()
       it('', function()
         model:clear()
         model:add_text(test1)
-        assert.is_equal(test1, model:get_text())
+        assert.same({ test1 }, model:get_text())
         model:cursor_left()
         model:cursor_left()
         local cc = model:get_cursor_x()
@@ -292,15 +295,14 @@ describe("input model spec", function()
 
       describe('backwards', function()
         local res = 'когда'
-        -- local res = 'когда'
         it('deletes', function()
           model:delete()
-          assert.is_equal('кога', model:get_text())
+          assert.same({ 'кога' }, model:get_text())
         end)
 
         it('does backspace', function()
           model:backspace()
-          assert.is_equal('коа', model:get_text())
+          assert.same({ 'коа' }, model:get_text())
         end)
 
         it('jumps home', function()
@@ -312,7 +314,7 @@ describe("input model spec", function()
 
         it('deletes', function()
           model:delete()
-          assert.is_equal('оа', model:get_text())
+          assert.same({ 'оа' }, model:get_text())
         end)
 
         it('jumps to the end', function()
@@ -324,7 +326,7 @@ describe("input model spec", function()
 
         it('does backspace', function()
           model:backspace()
-          assert.is_equal('о', model:get_text())
+          assert.same({ 'о' }, model:get_text())
         end)
       end)
     end)
