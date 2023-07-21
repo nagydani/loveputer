9d542143193d1ef21658dbe4234be8be2969d1f2
aldum <aldum@artixlinux.org>
2023-07-21 13:02:17 +0200

    feat(debug): handle self-referential tables

--- src/util/debug.lua
+++ src/util/debug.lua
@@ -1,5 +1,6 @@
 Debug = {
-  print_t = function(t, tag, ind)
+  print_t = function(t, tag, ind, prev_seen)
+    local seen = prev_seen or {}
     local indent = ind or 0
     local function get_indent()
       local dent = ''
@@ -16,19 +17,30 @@ Debug = {
       res = '[' .. tag .. ']'
     end
     if type(t) == 'table' then
+      if seen[t] then return end
+      seen[t] = true
       for k, v in pairs(t) do
-        local header = get_indent() .. '---- ' .. k .. ' ----\n'
-        res = res .. get_indent() .. header
-        res = res .. get_indent() .. Debug.print_t(v, nil, indent + 1)
-        res = res .. '\n'
+        local ts = Debug.print_t(v, nil, indent + 1, seen)
+        if ts then
+          local header = get_indent() .. '---- ' .. k .. ' ----\n'
+          res = res .. get_indent() .. header
+          res = res .. get_indent() .. ts
+          res = res .. '\n'
+        end
       end
     elseif type(t) == 'string' then
       res = res .. get_indent() .. t .. '\n'
     elseif type(t) == 'function' then
-      res = res .. get_indent() .. 'f() ' .. t .. '\n'
+      res = res .. get_indent() .. 'f() ' .. string.dump(t) .. '\n'
+      res = res .. get_indent() .. 'end\n'
     elseif type(t) == 'number' then
       res = res .. get_indent() .. 'N ' .. t .. '\n'
     end
     return res
   end
 }
+
+
+-- function Debug:dump_t(t)
+--   -- self.print_t(t)
+-- end
