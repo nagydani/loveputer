93b2cfd2bdc7c4cc1cf31bc1ab08f75eacb1f62c
aldum <aldum@artixlinux.org>
2023-07-26 10:48:34 +0200

    chore: set window to 1024x600

--- src/conf.lua
+++ src/conf.lua
@@ -5,12 +5,15 @@ function love.conf(t)
   if os.getenv("DEBUG") then
     love.DEBUG = true
   end
+
+  local width = 1024
+  local height = 600
   if hidpi == 'true' or hidpi == 'TRUE' then
-    t.window.width = 800 * 2
-    t.window.height = 480 * 2
+    t.window.width = width * 2
+    t.window.height = height * 2
     love.hiDPI = true
   else
-    t.window.width = 800
-    t.window.height = 480
+    t.window.width = width
+    t.window.height = height
   end
 end
