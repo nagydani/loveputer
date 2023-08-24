c84df40a8ce5e4b0687e2dee5616a28ee9bce42e
aldum <aldum@artixlinux.org>
2023-08-24 17:32:20 +0200

    feat: redirect io.write

--- src/model/ioRedirect.lua
+++ src/model/ioRedirect.lua
@@ -6,24 +6,38 @@ local function set_print(M)
   _G.orig_print = origPrint
   local magicPrint = function(...)
     local arg = { ... }
-    if type(arg) == 'string' then
-      -- origPrint('s', arg)
-      M.output:push({ arg .. '\n' })
+    -- origPrint('t', string.join(arg, '\t'))
+    for _, v in ipairs(arg) do
+      origPrint(v)
+      M.output:push(v)
     end
-    if type(arg) == 'table' then
-      -- origPrint('t', string.join(arg, '\t'))
-      for _, v in ipairs(arg) do
-        origPrint(v)
-        M.output:push(v)
-      end
-      -- M.output:push(arg)
-    end
+    -- M.output:push(arg)
   end
   _G.print = magicPrint
 end
 
+local function set_write(M)
+  if not M or not M.output or not M.output.write then
+    error('Cannot write to model')
+  end
+  local origWrite = io.write
+  local defaultOut = io.output()
+  local write = function(...)
+    origWrite(...)
+    local out = io.output()
+    if out == defaultOut then
+      local arg = { ... }
+      for _, v in ipairs(arg) do
+        M.output:write(v)
+      end
+    end
+  end
+  io.write = write
+end
+
 local function redirect_to(M)
   set_print(M)
+  set_write(M)
 end
 
 return redirect_to
