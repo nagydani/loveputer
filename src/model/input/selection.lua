8f7c6a33d118ded92b6c1d7e743a6ad909e78fb5
aldum <aldum@artixlinux.org>
2023-09-04 18:12:29 +0200

    feat: add isHeld selection field

--- src/model/input/selection.lua
+++ src/model/input/selection.lua
@@ -3,10 +3,18 @@ require("model.input.cursor")
 Selection = {}
 
 function Selection:new()
-  return {
+  local s = {
     start = {},
     fin = {},
     text = { '' },
     held = false,
   }
+  setmetatable(s, self)
+  self.__index = self
+
+  return s
+end
+
+function Selection:isHeld()
+  return self.held
 end
