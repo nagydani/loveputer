dcbbcd64b16d83b400e2aba38faa8098b8746606
aldum <aldum@artixlinux.org>
2023-11-05 13:03:25 +0100

    fix(util): guard against empty string in split

--- src/util/string.lua
+++ src/util/string.lua
@@ -127,17 +127,20 @@ end
 
 -- https://stackoverflow.com/a/51893646
 string.split = function(str, delimiter)
-  if not type(str) == 'string' then return {} end
-  local result               = {}
-  local from                 = 1
-  local delim_from, delim_to = string.find(str, delimiter, from)
-  while delim_from do
-    table.insert(result, string.sub(str, from, delim_from - 1))
-    from                 = delim_to + 1
-    delim_from, delim_to = string.find(str, delimiter, from)
+  if str and type(str) == 'string' and string.is_non_empty_string(str, true) then
+    local result               = {}
+    local from                 = 1
+    local delim_from, delim_to = string.find(str, delimiter, from)
+    while delim_from do
+      table.insert(result, string.sub(str, from, delim_from - 1))
+      from                 = delim_to + 1
+      delim_from, delim_to = string.find(str, delimiter, from)
+    end
+    table.insert(result, string.sub(str, from))
+    return result
+  else
+    return {}
   end
-  table.insert(result, string.sub(str, from))
-  return result
 end
 
 string.split_array = function(str_arr, char)
