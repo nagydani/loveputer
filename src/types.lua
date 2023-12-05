5ec11def03c4231412d8160fda951c84e484d125
aldum <aldum@artixlinux.org>
2023-12-05 19:11:35 +0100

    chore: add config type

--- src/types.lua
+++ src/types.lua
@@ -6,3 +6,17 @@
 ---| '"lua"'        # the default lua interpreter
 ---| '"input-text"' # plaintext user input
 ---| '"input-lua"'  # syntax highlighted lua input
+
+---@alias Fac # scaling
+---| 1
+---| 2
+
+--- @class ViewConfig table
+--- @field border integer
+--- @field font table
+--- @field fh integer -- font height
+--- @field fw integer -- font width
+--- @field lh integer -- line height
+--- @field FAC Fac
+--- @field h integer
+--- @field w integer
