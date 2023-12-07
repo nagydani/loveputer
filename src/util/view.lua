ad930a000af4ad41aa93b16e3d06f24cad3747d9
aldum <aldum@artixlinux.org>
2023-12-07 18:58:11 +0100

    refactor: move write_line to ViewUtils

--- src/util/view.lua
+++ src/util/view.lua
@@ -11,6 +11,18 @@ local get_drawable_height = function(cfg)
   return res
 end
 
+--- Write a line of text to output
+--- @param l number
+--- @param str string
+--- @param pos table
+--- @param cfg ViewConfig
+local write_line = function(l, str, pos, cfg)
+  local dy = pos.y - (-l + 1) * cfg.fh
+  G.setFont(cfg.font)
+  G.print(str, cfg.border, dy)
+end
+
 ViewUtils = {
   get_drawable_height = get_drawable_height,
+  write_line = write_line,
 }
--- src/view/interpreterView.lua
+++ src/view/interpreterView.lua
@@ -19,5 +19,38 @@ function InterpreterView:new(cfg, ctrl)
 end
 
 function InterpreterView:draw(input)
-  self.input:draw(input)
-end
+  local vd = self.controller:get_viewdata()
+  local time = self.controller:get_timestamp()
+
+  local isError = string.is_non_empty_string_array(vd.w_error)
+  local err_text = vd.w_error or {}
+
+  local colors = self.cfg.view.colors
+  local b = self.cfg.view.border
+  local fh = self.cfg.view.fh
+  local h = self.cfg.view.h
+
+  if isError then
+    local drawableWidth = self.cfg.drawableWidth
+    -- local drawableChars = self.cfg.drawableChars
+    local inLines = #err_text
+    local inHeight = inLines * fh
+    local apparentHeight = #err_text
+    local start_y = h - b - inHeight
+    local drawBackground = function()
+      G.setColor(colors.input.error_bg)
+      G.rectangle("fill",
+        b,
+        start_y,
+        drawableWidth,
+        apparentHeight * fh)
+    end
+    drawBackground()
+    G.setColor(colors.input.error)
+    for l, str in ipairs(err_text) do
+      ViewUtils.write_line(l, str, { y = start_y, breaks = #err_text - 1 }, self.cfg.view)
+    end
+  else
+    self.input:draw(input, time)
+  end
+end;
