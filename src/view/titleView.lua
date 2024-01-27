82a383a582168cc8bcc053956e6e2edf957e8201
aldum <aldum@artixlinux.org>
2024-01-27 12:42:40 +0100

    refactor: use G alias in TitleView

--- src/view/titleView.lua
+++ src/view/titleView.lua
@@ -1,20 +1,20 @@
 TitleView = {
   draw = function(title, x, y, w, custom_font)
     title = title or "LÖVEputer"
-    local prev_font = love.graphics.getFont()
+    local prev_font = G.getFont()
     local font = custom_font or prev_font
     local fh = font:getHeight()
     x = x or 0
-    y = y or love.graphics.getHeight() - 2 * fh
-    w = w or love.graphics.getWidth()
-    love.graphics.setColor(Color[0])
-    love.graphics.rectangle("fill", x, y, w, fh)
+    y = y or G.getHeight() - 2 * fh
+    w = w or G.getWidth()
+    G.setColor(Color[0])
+    G.rectangle("fill", x, y, w, fh)
     local i = 1
     local c = { 13, 12, 14, 10 }
     for lx = w - fh, w - 4 * fh, -fh do
-      love.graphics.setColor(Color[c[i]])
+      G.setColor(Color[c[i]])
       i = i + 1
-      love.graphics.polygon("fill",
+      G.polygon("fill",
         lx,
         y,
         lx - fh,
@@ -24,14 +24,14 @@ TitleView = {
         lx - fh,
         y + fh)
     end
-    love.graphics.setColor(Color[15])
+    G.setColor(Color[15])
 
     if custom_font then
-      love.graphics.setFont(font)
+      G.setFont(font)
     end
-    love.graphics.print(title, x + fh, y)
+    G.print(title, x + fh, y)
     if custom_font then
-      love.graphics.setFont(prev_font)
+      G.setFont(prev_font)
     end
   end
 }
