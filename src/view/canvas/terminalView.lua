86a794790adebe42274b024924ac0f842fa125c1
aldum <aldum@artixlinux.org>
2024-04-17 21:55:03 +0200

    fix: terminal overlay drawing

--- src/view/canvas/terminalView.lua
+++ src/view/canvas/terminalView.lua
@@ -8,76 +8,62 @@ TerminalView = {}
 local function terminal_draw(terminal, canvas, overlay)
   local char_width, char_height =
       terminal.char_width, terminal.char_height
-  if terminal.dirty then
-    love.graphics.push('all')
-    love.graphics.origin()
 
-    love.graphics.setCanvas(terminal.canvas)
-    -- love.graphics.clear(terminal.clear_color)
-    love.graphics.setFont(terminal.font)
-    local font_height = terminal.font:getHeight()
-    for y, row in ipairs(terminal.buffer) do
-      for x, char in ipairs(row) do
-        local state = terminal.state_buffer[y][x]
-        if state.dirty then
-          if overlay then
-            -- this clears everything permanently, wtf
-            -- love.graphics.clear(terminal.clear_color_alpha)
-            -- Log.once('overlay clear')
-          end
-          local left, top =
-              (x - 1) * char_width,
-              (y - 1) * char_height
-          local bg, fg = (function()
-            local back, fore
-            if state.reversed then
-              back, fore = state.color, state.backcolor
-            else
-              back, fore = state.backcolor, state.color
-            end
+  -- if terminal.dirty or overlay then
+  G.push('all')
+  G.origin()
 
-            if overlay then
-              back = Color.with_alpha(back, 0)
-              fore = Color.with_alpha(fore, 0.6)
-            end
-            return back, fore
-          end)()
+  G.setCanvas(canvas)
+  G.setFont(terminal.font)
+  G.clear(terminal.clear_color_alpha)
+  local font_height = terminal.font:getHeight()
 
-
-          -- Character background
-          if overlay then
-            -- love.graphics.setColor(unpack(terminal.clear_color_alpha))
-            -- love.graphics.rectangle("fill",
-            --     left, top + (font_height - char_height),
-            --     terminal.char_width, terminal.char_height)
-          else
-            love.graphics.setColor(unpack(bg))
-            love.graphics.rectangle("fill",
-              left, top + (font_height - char_height),
-              terminal.char_width, terminal.char_height)
-          end
-
-          local bm, am = love.graphics.getBlendMode()
-          G.setBlendMode('alpha', "alphamultiply")
-          -- Character
-          love.graphics.setColor(unpack(fg))
-          love.graphics.print(char, left, top)
-          Log.once('draw char')
-          -- if x == 1 and y == 1 and overlay then
-          if overlay then
-            Log.once('fg', unpack(fg))
-            Log.once('act fg', love.graphics.getColor())
-            Log.once('bg', love.graphics.getBackgroundColor())
-          end
-          G.setBlendMode(bm, am)
-
-          state.dirty = false
+  for y, row in ipairs(terminal.buffer) do
+    for x, char in ipairs(row) do
+      local state = terminal.state_buffer[y][x]
+      -- if state.dirty then
+      local left, top =
+          (x - 1) * char_width,
+          (y - 1) * char_height
+      local bg, fg = (function()
+        local back, fore
+        if state.reversed then
+          back, fore = state.color, state.backcolor
+        else
+          back, fore = state.backcolor, state.color
         end
+
+        if overlay then
+          back = Color.with_alpha(back, 0.25)
+          fore = Color.with_alpha(fore, 0.75)
+        end
+        return back, fore
+      end)()
+
+      -- Character background
+      if not overlay then
+        G.setColor(unpack(bg))
+        G.rectangle("fill",
+          left, top + (font_height - char_height),
+          char_width, char_height)
       end
+
+      local bm, am = G.getBlendMode()
+      G.setBlendMode('alpha', "alphamultiply")
+      -- Character
+      G.setColor(unpack(fg))
+      G.print(char, left, top)
+
+      G.setBlendMode(bm, am)
+
+      state.dirty = false
+      -- end
     end
-    terminal.dirty = false
-    love.graphics.pop()
   end
+  terminal.dirty = false
+  G.pop()
+  -- end
+
 
   if terminal.show_cursor then
     love.graphics.setFont(terminal.font)
@@ -95,8 +81,6 @@ function TerminalView.draw(terminal, canvas, snapshot)
   G.push('all')
 
   if snapshot then
-    -- G.setBlendMode('multiply', "premultiplied")
-    -- G.setBlendMode('screen', "premultiplied")
     terminal_draw(terminal, canvas, true)
   else
     terminal_draw(terminal, canvas)
