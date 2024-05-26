31880e3545fc8818eab71416fd49babd2bf81fb1
aldum <aldum@artixlinux.org>
2024-05-26 17:18:57 +0200

    feat: add input handlers to EditorController

--- src/controller/editorController.lua
+++ src/controller/editorController.lua
@@ -1,5 +1,6 @@
 --- @class EditorController
 --- @field model EditorModel
+--- @field input InputController
 --- @field open fun(self, name: string, content: string[]?)
 --- @field close fun(self): string[]
 EditorController = {}
@@ -14,6 +15,7 @@ setmetatable(EditorController, {
 --- @param M EditorModel
 function EditorController.new(M)
   local self = setmetatable({
+    input = InputController.new(M.interpreter.input),
     model = M,
   }, EditorController)
 
@@ -37,3 +39,20 @@ function EditorController:get_active_buffer()
   return self.model.buffer
 end
 
+--- @param t string
+function EditorController:textinput(t)
+  local interpreter = self.model.interpreter
+  if interpreter:has_error() then
+    interpreter:clear_error()
+  else
+    if Key.ctrl() and Key.shift() then
+      return
+    end
+    self.input:textinput(t)
+  end
+end
+
+--- @param k string
+function EditorController:keypressed(k)
+  self.input:keypressed(k)
+end
