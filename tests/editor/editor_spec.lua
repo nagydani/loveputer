9ccad8ef8bb40a1e7a340ec2e45cdfccce7ab760
aldum <aldum@artixlinux.org>
2024-06-04 15:10:38 +0200

    test(editor): add scroll tests

--- tests/editor/editor_spec.lua
+++ tests/editor/editor_spec.lua
@@ -50,7 +50,9 @@ describe('Editor', function()
       -- and it's an empty line, of course
       assert.same({}, sel_t)
     end)
+  end)
 
+  describe('works', function()
     describe('with wrap', function()
       local w = 16
       local mockConf = {
@@ -70,9 +72,10 @@ describe('Editor', function()
       controller:open('turtle', turtle_doc)
       view.buffer:open(model.buffer)
 
-      --- same as 1
-      it('works', function()
-        local buffer = controller:get_active_buffer()
+      local buffer = controller:get_active_buffer()
+      local start_sel = #turtle_doc + 1
+
+      it('opens', function()
         local bc = buffer:get_content()
 
         assert.same(turtle_doc, bc)
@@ -81,16 +84,18 @@ describe('Editor', function()
         local sel = buffer:get_selection()
         local sel_t = buffer:get_selected_text()
         -- default selection is at the end
-        assert.same({ #turtle_doc + 1 }, sel)
+        assert.same({ start_sel }, sel)
         -- and it's an empty line, of course
         assert.same({}, sel_t)
       end)
 
       --- additional tests
-      it('', function()
+      it('interacts', function()
         -- select middle line
         controller:keypressed('up')
+        assert.same({ start_sel - 1 }, buffer:get_selection())
         controller:keypressed('up')
+        assert.same({ start_sel - 2 }, buffer:get_selection())
         assert.same({ turtle_doc[2] }, model.buffer:get_selected_text())
         -- load it
         local input = function()
@@ -100,6 +105,7 @@ describe('Editor', function()
         assert.same({ turtle_doc[2] }, input())
         -- moving selection clears input
         controller:keypressed('down')
+        assert.same({ start_sel - 1 }, buffer:get_selection())
         assert.same({ '' }, input())
         -- add text
         controller:textinput('t')
@@ -108,7 +114,87 @@ describe('Editor', function()
         controller:textinput('s')
         controller:textinput('t')
         assert.same({ 'test' }, input())
+        -- replace line with input content
+        controller:keypressed('return')
+        assert.same({ '' }, input())
+        local bc = buffer:get_content()
+        assert.same('test', bc[3])
+        -- replace
+        controller:textinput('i')
+        controller:textinput('n')
+        controller:textinput('s')
+        controller:textinput('e')
+        controller:textinput('r')
+        controller:textinput('t')
+        assert.same({ 'insert' }, input())
+        controller:keypressed('escape')
+        assert.same({ 'test' }, input())
       end)
+    end)
+
+    it('with scroll', function()
+      local l = 6
+      local mockConf = {
+        view = {
+          lines = l,
+          drawableChars = 80,
+        },
+      }
+      local sierpinski = {
+        "function sierpinski(depth)",
+        "  lines = { '*' }",
+        "  for i = 2, depth + 1 do",
+        "    sp, tmp = string.rep(' ', 2 ^ (i - 2))",
+        "    tmp = {}",
+        "    for idx, line in ipairs(lines) do",
+        "      tmp[idx] = sp .. line .. sp",
+        "      tmp[idx + #lines] = line .. ' ' .. line",
+        "    end",
+        "    lines = tmp",
+        "  end",
+        "  return table.concat(lines, '\n')",
+        "end",
+        "",
+        "print(sierpinski(4))",
+      }
+
+      local model = EditorModel(mockConf)
+      local controller = EditorController(model)
+      local view = EditorView(mockConf.view, controller)
+
+      controller:open('sierpinski.lua', sierpinski)
+      view.buffer:open(model.buffer)
+
+      local visible = view.buffer.content
+      local scroll = view.buffer.SCROLL_BY
+
+      -- inital scroll is at EOF, meaning last l lines are visible
+      -- plus the phantom line
+      local off = #sierpinski - l + 1
+      assert.same(off, view.buffer.offset)
+      local start_range = Range(off + 1, #sierpinski + 1)
+      assert.same(start_range, visible.range)
+
+      controller:keypressed('pageup')
+      assert.same(start_range:translate(-scroll), visible.range)
+      controller:keypressed('pageup')
+      assert.same(start_range:translate(-scroll * 2), visible.range)
+      controller:keypressed('pageup')
+      assert.same(start_range:translate(-scroll * 3), visible.range)
+      controller:keypressed('pageup')
+      local base = Range(1, l)
+      assert.same(base, visible.range)
+      controller:keypressed('pagedown')
+      assert.same(base:translate(scroll), visible.range)
+      controller:keypressed('pagedown')
+      assert.same(base:translate(scroll * 2), visible.range)
+      controller:keypressed('pagedown')
+      assert.same(base:translate(scroll * 3), visible.range)
+      controller:keypressed('pagedown')
+      assert.same(base:translate(scroll * 4), visible.range)
+      controller:keypressed('pagedown')
+      local limit = #sierpinski + visible.overscroll
+      assert.same(Range(limit - l + 1, limit), visible.range)
     end)
   end)
 end)
