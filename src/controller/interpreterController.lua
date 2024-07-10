704aea43c79ae32c2c49d20168971972699a82a2
aldum <aldum@artixlinux.org>
2024-07-10 15:57:58 +0200

    refactor: wrap some InputCtrl functions in InterpreterCtrl

--- src/controller/interpreterController.lua
+++ src/controller/interpreterController.lua
@@ -1,6 +1,18 @@
 --- @class InterpreterController
 --- @field model InterpreterModel
 --- @field input InputController
+---
+--- @field set_eval fun(self, EvalBase)
+--- @field get_eval fun(self): EvalBase
+--- @field get_viewdata fun(self): ViewData
+--- @field set_text fun(self, t: string|string[])
+--- @field add_text fun(self, t: string|string[])
+--- @field textinput fun(self, t: string)
+--- @field keypressed fun(self, k: string): boolean?
+--- @field clear fun(self)
+--- @field get_input fun(self): InputDTO
+--- @field get_text fun(self): string[]
+--- @field set_custom_status fun(self, CustomStatus)
 InterpreterController = {}
 InterpreterController.__index = InterpreterController
 
@@ -21,9 +33,59 @@ function InterpreterController.new(model, input)
   return self
 end
 
+--- @param eval EvalBase
+function InterpreterController:set_eval(eval)
+  self.input:set_eval(eval)
+end
+
+--- @return EvalBase
+function InterpreterController:get_eval()
+  return self.input.model.evaluator
+end
+
 --- @return ViewData
 function InterpreterController:get_viewdata()
   return {
     w_error = self.model:get_wrapped_error(),
   }
+end
+
+--- @param t string|string[]
+function InterpreterController:set_text(t)
+  self.input:set_text(t)
+end
+
+--- @param t string|string[]
+function InterpreterController:add_text(t)
+  self.input:add_text(t)
+end
+
+--- @param t string
+function InterpreterController:textinput(t)
+  self.input:textinput(t)
+end
+
+--- @param k string
+--- @return boolean?
+function InterpreterController:keypressed(k)
+  return self.input:keypressed(k)
+end
+
+function InterpreterController:clear()
+  self.input:clear()
+end
+
+--- @return InputDTO
+function InterpreterController:get_input()
+  return self.input:get_input()
+end
+
+--- @return string[]
+function InterpreterController:get_text()
+  return self:get_input().text
+end
+
+--- @param cs CustomStatus
+function InterpreterController:set_custom_status(cs)
+  self.input:set_custom_status(cs)
 end
