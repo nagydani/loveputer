4112b5eba7e05e10d86bb4fe33bc375f09de7835
aldum <aldum@artixlinux.org>
2024-07-20 16:26:11 +0200

    feat(editor): add VisibleBlock

---
+++ src/view/editor/visibleBlock.lua
@@ -0,0 +1,50 @@
+require("util.range")
+require("util.string")
+
+--- @class VisibleBlock
+--- @field wrapped WrappedText
+--- @field highlight SyntaxColoring
+--- @field pos Range
+VisibleBlock = {}
+VisibleBlock.__index = VisibleBlock
+
+setmetatable(VisibleBlock, {
+  __call = function(cls, ...)
+    return cls.new(...)
+  end,
+})
+
+--- @param w integer
+--- @param lines string|string[]
+--- @param pos Range
+--- @return VisibleBlock
+function VisibleBlock.new(w, lines, hl, pos)
+  local ls = (function()
+    if type(lines) == 'string' then return { lines } end
+    return lines
+  end)()
+
+  local wrapped_text = WrappedText(w, ls)
+  local wrapped_highlight = {}
+  if wrapped_text.n_breaks > 0 then
+    for ln, line in ipairs(wrapped_text.text) do
+      local rln = wrapped_text.wrap_reverse[ln]
+      wrapped_highlight[ln] = {}
+      for i = 1, string.ulen(line) do
+        local rank = wrapped_text.wrap_rank[ln]
+        local c = i + rank * w
+        wrapped_highlight[ln][i] = hl[rln][c]
+      end
+    end
+  else
+    wrapped_highlight = hl
+  end
+
+  local self = setmetatable({
+    wrapped = wrapped_text,
+    highlight = wrapped_highlight,
+    pos = pos,
+  }, VisibleBlock)
+
+  return self
+end
