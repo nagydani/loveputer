c07fc24dc294a940a9a7e2651d55e3d3c52756c2
aldum <aldum@artixlinux.org>
2024-09-15 18:13:16 +0200

    feat(debug): add Cursor tostring()

--- src/model/input/selection.lua
+++ src/model/input/selection.lua
@@ -9,6 +9,7 @@ InputSelection = {}
 
 function InputSelection:new()
   local s = {
+    --- TODO refactor this into a Range
     start = nil,
     fin = nil,
     text = { '' },
@@ -22,4 +23,13 @@ end
 
 function InputSelection:is_held()
   return self.held
+end
+
+function InputSelection:__tostring()
+  local s = self.start
+  local e = self.fin
+  local held = (function()
+    if self.held then return 'v' else return '^' end
+  end)()
+  return string.format('{%d-%d}[%d]', s, e, held)
 end
