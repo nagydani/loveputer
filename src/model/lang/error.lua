1a6922c96f008bcaca9b561ee303ec65d1144dae
aldum <aldum@artixlinux.org>
2024-09-24 19:27:26 +0200

    feat: EvalError utilities

--- src/model/lang/error.lua
+++ src/model/lang/error.lua
@@ -7,14 +7,34 @@ local class = require('util.class')
 --- @field msg string
 --- @field c number?
 --- @field l number
+---
+--- @field wrap fun(e: string|EvalError): EvalError?
+--- @field __tostring function
 
 --- @param msg string
 --- @param c number?
 --- @param l number?
 local newe = function(msg, c, l)
-  local ll = l or 1
-  return { msg = msg, c = c, l = ll }
+  return { msg = msg, c = c, l = l }
 end
 
 --- @type EvalError
 EvalError = class.create(newe)
+
+function EvalError.wrap(e)
+  if type(e) == "string" then
+    return EvalError(e)
+  end
+  if type(e) == "table" and type(e.msg) == "string" then
+    return e
+  end
+  return EvalError(tostring(e))
+end
+
+function EvalError:__tostring()
+  local li = ''
+  if self.l and self.c then
+    li = string.format('L%d:%d:', self.l, self.c)
+  end
+  return li .. self.msg
+end
