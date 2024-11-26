3c2a3eaebe7a57d88166a1d2c10aeec3d98f7eb4
aldum <aldum@artixlinux.org>
2024-11-26 14:05:01 +0100

    feat(search): controller

---
+++ src/controller/searchController.lua
@@ -0,0 +1,10 @@
+local class = require('util.class')
+
+--- @class SearchController
+SearchController = class.create(function()
+  return {}
+end)
+
+--- @param k string
+function SearchController:keypressed(k)
+end
